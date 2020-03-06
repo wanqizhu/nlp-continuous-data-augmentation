8f060ff1552242918b3f171aee948a1dbbbc078b
Wanqi Zhu <1213.ghs@gmail.com>
2020-03-05 18:10:38 -0800

    swapdim

--- data_augmenter.py
+++ data_augmenter.py
@@ -54,3 +54,24 @@ class NoisyIdentityDataAugmenter(BaseDataAugmenter):
         return list(zip(sentences, sentiments))
 
 
+
+class EmbedDimensionSwapDataAugmenter(BaseDataAugmenter):
+    def __init__(self, nswaps=1, niters=3):
+        super().__init__()
+        self.nswaps = int(nswaps)
+        self.niters = niters
+
+    def augment(self, embedded_training_data):
+        sentences, sentiments = embedded_training_data
+
+        def swap(s):
+            for _ in range(self.nswaps):
+                dim1 = np.random.randint(s.shape[1])
+                dim2 = np.random.randint(s.shape[1])
+                s[:, dim1], s[:, dim2] = s[:, dim2], s[:, dim1].copy()
+            return s
+
+        sentences = sentences + [swap(s) for s in (sentences*self.niters)]
+        sentiments = sentiments + [s for s in (sentiments * self.niters)]
+
+        return list(zip(sentences, sentiments))
